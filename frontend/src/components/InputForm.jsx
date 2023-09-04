c419efec6facad4cb7dd5d3f10f70ab7f3614685
Anosha Rahim <anosharahim03@gmail.com>
2023-09-04 13:29:55 -0700

    create inputform component

---
+++ frontend/src/components/InputForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function InputForm() {
+  const [url, setUrl] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://your-django-backend-url/url_input/",
+        { url }
+      );
+      setResponse(`Entered URL: ${response.data.url}`);
+    } catch (error) {
+      console.error(error);
+      setResponse("Error occurred while sending the URL.");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter URL:
+          <input
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <p>{response}</p>
+    </div>
+  );
+}
+
+export default InputForm;
