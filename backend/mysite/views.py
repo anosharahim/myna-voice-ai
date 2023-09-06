1e54082dd418476304833bac8e1a5daf13f89082
Anosha Rahim <anosharahim03@gmail.com>
2023-09-05 17:36:50 -0700

    fix signal threading issue by disabling signal

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import trafilatura
+from trafilatura.settings import use_config
+newconfig = use_config()
+newconfig.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
 
 class TextSearchView(APIView):
     def post(self, request):
-        # url = ''
+
         if request.method == 'POST':
             url = request.data.get('url', '')
             main_content = trafilatura.fetch_url(url)
-            extracted_text = trafilatura.extract(main_content, output_format="text")
+            extracted_text = trafilatura.extract(main_content, output_format="text", config=newconfig)
     
             return Response({'url': url, 'main_content': extracted_text}, status=200)
