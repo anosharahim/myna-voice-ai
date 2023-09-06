29a43910e65fe7f5b501671a8a3efcb552a93e42
Anosha Rahim <anosharahim03@gmail.com>
2023-09-05 17:36:15 -0700

    extract url content using trafilatura

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import trafilatura
 
 class TextSearchView(APIView):
     def post(self, request):
         # url = ''
         if request.method == 'POST':
             url = request.data.get('url', '')
-            
-        return Response({'url': url}, status=200)
+            main_content = trafilatura.fetch_url(url)
+            extracted_text = trafilatura.extract(main_content, output_format="text")
+    
+            return Response({'url': url, 'main_content': extracted_text}, status=200)
