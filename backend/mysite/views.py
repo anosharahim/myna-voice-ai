1248f22b80f123ce2bda838cf5bba210612e553a
Anosha Rahim <anosharahim03@gmail.com>
2023-09-08 13:50:19 -0700

    convert extracted text to audio file

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -10,28 +10,33 @@ from TTS.api import TTS
 newconfig = use_config()
 newconfig.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
 
+
 class TextSearchView(APIView):
     def post(self, request):
 
         if request.method == 'POST':
             url = request.data.get('url', '')
             main_content = trafilatura.fetch_url(url)
-            extracted_text = trafilatura.extract(main_content, output_format="text", config=newconfig)
-    
+            extracted_text = trafilatura.extract(
+                main_content, output_format="text", config=newconfig)
+            wav_file = text_to_audio(extracted_text[:1024])
+
             return Response({'url': url, 'main_content': extracted_text}, status=200)
-        
-    
-def text_to_audio(self, content): 
+
+
+def text_to_audio(content):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     model_name = TTS().list_models()[0]
     tts = TTS(model_name).to(device)
 
-    #this gives numpy output
+    # this gives numpy output
     wav = tts.tts(content, speaker=tts.speakers[0], language=tts.languages[0])
+    print("wav done", len(wav))
+    # print(wav)
 
-    #this gives a .wav audio file
+    # this gives a .wav audio file
     # audio_file = tts.tts_to_file(text="Hello world!", speaker=tts.speakers[0], language=tts.languages[0], file_path="output.wav")
 
     return wav
