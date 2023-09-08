d87d1901a6764a83c95c2e9abba2a419654b0a6d
Anosha Rahim <anosharahim03@gmail.com>
2023-09-08 16:36:10 -0700

    create database model for audio files (save local)

--- backend/mysite/admin.py
+++ backend/mysite/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import TextLibrary
 
+
 class Admin(admin.ModelAdmin):
-    list_display = ('title', 'url', 'text_content')
+    list_display = ('title', 'audio_id', 'website_url', 'text_content')
 
 # Register your models here.
+
 
 admin.site.register(TextLibrary, Admin)
--- backend/mysite/models.py
+++ backend/mysite/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class TextLibrary(models.Model):
     title = models.CharField(max_length=120)
-    url = models.URLField()
-    text_content = models.TextField()
+    audio_id = models.TextField(default=None)
+    website_url = models.URLField()
+    text_content = models.TextField(null=True)
 
     def __str__(self):
         return self.title
