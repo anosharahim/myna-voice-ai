264846fe32fb11ab6207630b870f44b6f40780e6
Anosha Rahim <anosharahim03@gmail.com>
2023-09-20 15:54:32 -0700

    create user model

--- backend/mysite/admin.py
+++ backend/mysite/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import TextLibrary
+from .models import TextLibrary, UserAccount
 
 
-class Admin(admin.ModelAdmin):
+class TextAdmin(admin.ModelAdmin):
     list_display = ('title', 'audio_id', 'website_url', 'text_content')
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password')
+
+
 # Register your models here.
-
-
-admin.site.register(TextLibrary, Admin)
+admin.site.register(TextLibrary, TextAdmin)
+admin.site.register(UserAccount, UserAdmin)
--- backend/mysite/models.py
+++ backend/mysite/models.py
@@ -9,3 +9,11 @@ class TextLibrary(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserAccount(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.username
