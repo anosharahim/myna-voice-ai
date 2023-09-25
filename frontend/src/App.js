de8447b414fe1cf85cf7ae65c166f3c92c75aafb
Anosha Rahim <anosharahim03@gmail.com>
2023-09-25 13:15:14 -0700

    account login or sign up logic to main page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import InputForm from "./components/InputForm";
 import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleAccountCreation = () => {
+    // go to sign-up page
+    setIsSignUp(true);
+  };
+  // Function to handle successful sign-in or sign-up
+  const handleAuthentication = () => {
+    setIsAuthenticated(true);
+  };
+
   return (
     <div className="App">
-      <h1>Paste URL for text you want to listen to:</h1>
-      <InputForm />
-      <SignUpForm />
+      <div className="App">
+        {isAuthenticated ? (
+          <InputForm />
+        ) : isSignUp ? (
+          <SignUpForm onAuthentication={handleAuthentication} />
+        ) : (
+          <>
+            <SignInForm onAuthentication={handleAuthentication} />
+            <button
+              onClick={handleAccountCreation}
+              className="btn"
+              type="submit"
+            >
+              {" "}
+              Create Account
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
