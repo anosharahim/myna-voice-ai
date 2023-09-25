33326da16f030635725d1159d0194a472c75d699
Anosha Rahim <anosharahim03@gmail.com>
2023-09-25 15:01:58 -0700

    disable csrf middleware

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -71,3 +71,12 @@ def check_is_authenticated(request):
     else:
         return JsonResponse({"error": "not logged in"}, status=403)
 
+class DisableCSRFMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        setattr(request, '_dont_enforce_csrf_checks', True)
+        response = self.get_response(request)
+        return response
