98d232b9e7e35908b9bae7bc4719525b43fb728f
Anosha Rahim <anosharahim03@gmail.com>
2023-09-25 13:14:50 -0700

    create sign in form

---
+++ frontend/src/components/SignInForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function SignUpForm() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [response, setResponse] = useState("");
+
+  // Handling the name change
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/login/", {
+        name,
+        password,
+      });
+      setResponse(`http://127.0.0.1:8000/${response.data}`);
+      setSubmitted(true);
+    } catch (error) {
+      console.error(error);
+      setResponse("Error occurred while logging in.");
+    }
+  };
+  return (
+    <div className="form">
+      <div>
+        <h1>User SignIn</h1>
+      </div>
+      <form>
+        {/* Labels and inputs for form data */}
+        <label className="label">Name</label>
+        <input
+          onChange={handleName}
+          className="input"
+          value={name}
+          type="text"
+        />
+
+        <label className="label">Password</label>
+        <input
+          onChange={handlePassword}
+          className="input"
+          value={password}
+          type="password"
+        />
+
+        <button onClick={handleSubmit} className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
