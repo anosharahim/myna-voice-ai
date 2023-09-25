e6956bfebf36939de7973ef2981c256031a15325
Anosha Rahim <anosharahim03@gmail.com>
2023-09-25 15:00:22 -0700

    disable csrf

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -33,11 +33,13 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'mysite.views.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
