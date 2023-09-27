721bcc5176f54c6b86e970e2e8194e6979a6372e
Anosha Rahim <anosharahim03@gmail.com>
2023-09-26 17:02:37 -0700

    link user model to text library

--- backend/mysite/models.py
+++ backend/mysite/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class TextLibrary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=120)
     audio_id = models.TextField(default=None)
     website_url = models.URLField()
