d4a32a99ab755a859f645e2dcb049f64afd1bf77
Anosha Rahim <anosharahim03@gmail.com>
2023-09-26 17:02:20 -0700

    register text admin model with user field

--- backend/mysite/admin.py
+++ backend/mysite/admin.py
@@ -3,12 +3,9 @@ from .models import TextLibrary
 
 
 class TextAdmin(admin.ModelAdmin):
-    list_display = ('title', 'audio_id', 'website_url', 'text_content')
+    list_display = ('user', 'title', 'audio_id',
+                    'website_url', 'text_content')
 
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('username', 'password')
-
-
-# Register your models here.
+# Register models here.
 admin.site.register(TextLibrary, TextAdmin)
