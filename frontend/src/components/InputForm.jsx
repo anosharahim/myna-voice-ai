78c34e35e30a87ede0360ffec8c14f9c010d7639
Anosha Rahim <anosharahim03@gmail.com>
2023-10-13 15:46:16 -0700

    separate audioplayer from parent component

--- frontend/src/components/InputForm.jsx
+++ frontend/src/components/InputForm.jsx
@@ -1,55 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.continuous = true;
-recognition.interimResults = true;
+import AudioPlayer from "./AudioPlayer";
 
 function InputForm({ onLogOut }) {
   const [url, setUrl] = useState("");
   const [response, setResponse] = useState("");
-  const audioElementRef = useRef(null);
-
-  // Start listening when component gets rendered, and never stop unless component gets removed from page
-  useEffect(() => {
-    let timeout = null;
-
-    recognition.onstart = () => {
-      console.log("Speech recognition started");
-    };
-    recognition.onend = () => {
-      console.log("Speech recognition ended, WHY?");
-    };
-    recognition.onerror = (event) => {
-      console.error("Speech recognition error:", event.error);
-    };
-
-    recognition.onresult = (event) => {
-      const transcript = event.results[0][0].transcript;
-      console.log(transcript);
-      // Use the audioElementRef to pause the audio
-      if (audioElementRef.current) {
-        audioElementRef.current.pause();
-      }
-      // For later: send text to backend, inject into prompt, get anser from OpenAI API, send back to frontend
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        if (audioElementRef.current) {
-          audioElementRef.current.play();
-        }
-      }, 3000);
-    };
-
-    return () => {
-      recognition.stop();
-    };
-  }, []);
-
-  const handleStart = () => {
-    recognition.start();
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -88,10 +43,10 @@ function InputForm({ onLogOut }) {
         <button type="submit">Submit</button>
       </form>
       <button onClick={handleLogOut}> Logout</button>
-      <button onClick={handleStart}>Start Listening</button>
       {response ? (
-        <audio autoPlay controls src={response} ref={audioElementRef}></audio>
+        <AudioPlayer response={response} />
       ) : (
+        // {/* <audio controls src={response} ref={audioElementRef}></audio> */}
         <div> no file yet </div>
       )}
     </div>
