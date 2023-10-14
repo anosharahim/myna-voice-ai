251960620385083020399d99aea8d7f6ef09fb9c
Anosha Rahim <anosharahim03@gmail.com>
2023-10-14 15:48:16 -0700

    make url field unique

--- backend/mysite/models.py
+++ backend/mysite/models.py
@@ -6,7 +6,7 @@ class TextLibrary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=120)
     audio_id = models.TextField(default=None)
-    website_url = models.URLField()
+    website_url = models.URLField(unique=True)
     text_content = models.TextField(null=True)
 
     def __str__(self):
