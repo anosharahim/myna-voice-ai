233ce0a5276cdce21bea3e256bb72dda67e0cac6
Anosha Rahim <anosharahim03@gmail.com>
2023-11-27 17:14:19 -0800

    create site header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="site-header">
+      <div className="site-name">Beak</div>
+    </header>
+  );
+};
+
+export default Header;
