d2fe09ab69c44c3862a1a89f2c3989debe35bd0b
Anosha Rahim <anosharahim03@gmail.com>
2023-11-28 13:22:05 -0800

    add volume slider to desktop

--- frontend/src/components/AudioPlayer.jsx
+++ frontend/src/components/AudioPlayer.jsx
@@ -11,22 +11,32 @@ const AudioPlayer = () => {
 
   return (
     <div className="audio-pop-up">
-      <div className="play-controls">
-        <button className="rewind-button"></button>
-        <button
-          className={isPlaying ? "pause-button" : "play-button"}
-          onClick={togglePlay}
-        ></button>
-        <button className="fastforward-button"></button>
-      </div>
-      <div className="slider-with-time">
-        <div className="start-time">00.00</div>
-        <div className="slider">
-          <button className="slider-circle"></button>
-          <div className="slider-thin-line"></div>
-          <div className="slider-thick-line"></div>
+      <div className="curr-audio-title">1. This is an audio title</div>
+      <div className="audio-center-controls">
+        <div className="play-controls">
+          <button className="rewind-button"></button>
+          <button
+            className={isPlaying ? "pause-button" : "play-button"}
+            onClick={togglePlay}
+          ></button>
+          <button className="fastforward-button"></button>
         </div>
-        <div className="end-time">00.00</div>
+        <div className="slider-with-time">
+          <div className="start-time">00.00</div>
+          <div className="slider">
+            <button className="slider-circle"></button>
+            <div className="slider-thin-line"></div>
+            <div className="slider-thick-line"></div>
+          </div>
+          <div className="end-time">00.00</div>
+        </div>
+      </div>
+      <div className="volume">
+        <button className="volume-button"></button>
+        <div className="volume-slider">
+          <div className="volume-thin-line"></div>
+          <div className="volume-thick-line"></div>
+        </div>
       </div>
     </div>
   );
