92ee0b1000491410fabd81fe8006650687901e5b
anosharahim <anosharahim03@gmail.com>
2024-04-15 12:54:44 -0700

    update aws settings

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -150,6 +150,10 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_URL_PROTOCOL = 'https'
 AWS_S3_USE_SSL = True
 AWS_S3_VERIFY = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
 # Static and media file configuration
 STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/static/'
