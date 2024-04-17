3f1fa757c6f8ff37b85c9c7a9bf6fe2fb5cf4023
anosharahim <anosharahim03@gmail.com>
2024-04-16 21:22:27 -0700

    handle delete audios backend logic

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,17 +16,18 @@ from django.http import JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 import json
 import speech_recognition as sr
-# from config import OPENAI_GPT4_KEY
 import openai
 # import requests
 from bs4 import BeautifulSoup
 from pydub import AudioSegment
 import os
+from dotenv import load_dotenv
+import boto3
 from django.core.files.storage import default_storage
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
-import boto3
+
 
 newconfig = use_config()
 newconfig.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
@@ -120,16 +122,40 @@ class AudioLibraryView(APIView):
             user_audios = AudioItem.objects.filter(
                 user=user).values('title', 'audio_id')
             audio_library_data = [{'title': audio['title'],
+                                   'audio_id': audio['audio_id'],
                                 'url': f"{settings.STATIC_URL}uploads/{audio['audio_id']}.wav"} for audio in user_audios]
             return Response({'audio_library_data': audio_library_data}, status=200)
         else: 
             return Response({'error': 'User not authenticated'}, status=401)
 
+
+# Load environment variables from .env file
+load_dotenv()
+# Access the environment variables
+aws_access_key_id = os.getenv("S3_KEY")
+aws_secret_access_key = os.getenv("S3_SECRET")
+
 @require_http_methods(["DELETE"])       
 def delete_audio(request):
     try:
-        audio_url = request.GET.get('audio_to_delete')
+    
+        audio_id = request.GET.get('audio_to_delete')
+        audio_url = f'https://s3-{settings.AWS_S3_REGION_NAME}.amazonaws.com/{settings.AWS_STORAGE_BUCKET_NAME}uploads/{audio_id}.wav'
+
+        #delete from s3
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            region_name=settings.AWS_S3_REGION_NAME
+        )
+        object_key = f'uploads/{audio_id}.wav'
+        s3_client.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=object_key)
+
+        AudioItem.objects.filter(audio_id=audio_id).delete() #delete from DB
+        
         return JsonResponse({'message': 'Audio deleted successfully'})
+        
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
     
